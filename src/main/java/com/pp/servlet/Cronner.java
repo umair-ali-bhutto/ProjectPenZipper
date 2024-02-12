0649967d59477cf65883b32f0cb705369610487a
umair-ali-bhutto <umair2101f@aptechgdn.net>
2024-02-12 11:25:47 +0500

    Added Emoji And Content Type HTML

--- src/main/java/com/pp/servlet/Cronner.java
+++ src/main/java/com/pp/servlet/Cronner.java
@@ -21,7 +21,7 @@ import com.pp.util.Prop;
  * @author umair.ali
  * @version 1.0
  * 
- * Servlet implementation class Cronner Will Run Cron Jobs
+ *          Servlet implementation class Cronner Will Run Cron Jobs
  */
 @WebServlet("/")
 public class Cronner extends HttpServlet {
@@ -51,10 +51,21 @@ public class Cronner extends HttpServlet {
 			throws ServletException, IOException {
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
-			DoPrint.logInfo("Cron Job Scheduled! \n At:" + sdf.format(new Date()) + " \nWorking Directory is:"
-					+ System.getProperty("user.dir")+"\nKindly Keep Your .env File On This Path As Told In README.md\nCome On You Have "+Long.parseLong(Prop.getProperty("timer.delay"))+" Seconds.(If Already Kept Kindly Ignore This @UMAIR.ALI)");
-			response.getWriter().append("Cron Job Scheduled! \n At:" + sdf.format(new Date()) + " \nWorking Directory is:"
-					+ System.getProperty("user.dir")+"\nKindly Keep Your .env File On This Path As Told In README.md");
+			DoPrint.logInfo("Cron Job Scheduled! \nAt:" + sdf.format(new Date()) + " \nWorking Directory is:"
+					+ System.getProperty("user.dir")
+					+ "\nKindly Keep Your .env File On This Path As Told In README.md\nCome On You Have "
+					+ Long.parseLong(Prop.getProperty("timer.delay"))
+					+ " Seconds.(If Already Kept Kindly Ignore This 🔥@UMAIR.ALI🔥)");
+
+			response.setContentType("text/html; charset=UTF-8");
+			response.getWriter()
+					.append("Cron Job Scheduled! <br>At:" + sdf.format(new Date()) + " <br>Working Directory is:"
+							+ System.getProperty("user.dir")
+							+ "<br>Kindly Keep Your .env File On This Path As Told In README.md<br>Come On You Have "
+							+ Long.parseLong(Prop.getProperty("timer.delay"))
+							+ " Seconds.(If Already Kept Kindly Ignore This &#128293;@UMAIR.ALI&#128293;)<br>"
+							+ "<a href=\"https://github.com/umair-ali-bhutto\" target=\"_blank\">@Github</a><br>"
+							+ "<a href=\"mailto:umair2101f@aptechgdn.net\">@Email (umair2101f@aptechgdn.net)</a>");
 		} catch (Exception e) {
 			response.getWriter().append("Something Went Wrong.");
 		}
